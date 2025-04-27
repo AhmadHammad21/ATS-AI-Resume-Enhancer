7b602f640dcf15d4b935cd42ec9a6f17905f7745
Ahmad Hammad <ahmedhammad8765@gmail.com>
2025-04-27 23:16:24 +0300

    Having only one CV

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ st.set_page_config(page_title="ATS Resume Expert")
 
 def main():
     st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ["Main Page", "Upload Multiple CV's", "More Features", "Custom Prompt"])
+    # selection = st.sidebar.radio("Go to", ["Main Page", "Upload Multiple CV's", "More Features", "Custom Prompt"])
+    selection = st.sidebar.radio("Go to", ["Upload Multiple CV's"])
 
-    if selection == "Main Page":
-        main_page()
+    # if selection == "Main Page":
+    #     main_page()
     if selection == "Upload Multiple CV's":
         upload_multiple_page()
-    if selection == "More Features":
-        more_features_page()
-    if selection == "Custom Prompt":
-        custom_prompt()
+    # if selection == "More Features":
+    #     more_features_page()
+    # if selection == "Custom Prompt":
+    #     custom_prompt()
 
 if __name__ == "__main__":
     main()
